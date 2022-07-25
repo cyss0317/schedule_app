33bb995cd859eb66f2cb2aa0048cbfa3c07d214c
Yun Choi <cyss0317@gmail.com>
2022-07-24 23:14:38 -0500

    Post route created

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -6,20 +6,30 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class Person(BaseModel):
+class User(BaseModel):
     id: int
     name: str
-    age: int
+    email: str
+    birth_day: str
+    preferred_days: list[str]
+    roles: Optional[list[str]]
 
 
-DB: List[Person] = [
-  Person(id=1, name="Yun", age=33)
+DB: List[User] = [
+    User(id=1, name="Yun", email="cyss0317@yahoo.com",
+         birth_day="03/17/1990", preferred_days=["mon", "tue"]),
+    User(id=2, name="Jason", email="cyss0317@yahoo.com",
+         birth_day="03/17/1990", preferred_days=["mon", "tue"])
 ]
 
 
 @app.get("/")
 def read_root():
-    return DB
+    answer = ""
+    # put each user from the DB and return all the names
+    for user in DB:
+        answer += f"Hello, {user.name}. Your email is {user.email}"
+    return answer
 
 
 @app.get("/{id}")
@@ -28,4 +38,16 @@ def get_person(id: int):
         if person.id == id:
             return person
 
-            
+
+@app.post("/user")
+def create_user(user: User):
+    DB.append(user)
+    return user
+
+
+@app.delete("/delete/{id}")
+def delete_person(id: int):
+    for person in DB:
+        if person.id == id:
+            DB.remove(person)
+            return f"Delete {person.name} successfully"
